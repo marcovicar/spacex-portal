44a1c3ebf93650819008d65b0b3be63993383b93
marcovicar <marcosvictormbi85@gmail.com>
2025-07-20 23:51:05 +0100

    ✅ Changes on SkeletonLaunchCard

--- src/components/custom/SkeletonLaunchCard.tsx
+++ src/components/custom/SkeletonLaunchCard.tsx
@@ -1,17 +1,17 @@
-import {Skeleton} from "@/components/ui/skeleton";
+import { Skeleton } from "@/components/ui/skeleton"
 
-export function SkeletonLaunchCard() {
+export default function SkeletonLaunchCard() {
   return (
-    <div className="border border-zinc-800 rounded-md shadow-md overflow-hidden">
-      <Skeleton className="w-full h-64 rounded-t-md" />
+    <div className="bg-black w-full text-white border border-zinc-800 shadow-md rounded-md overflow-hidden animate-pulse">
+      <Skeleton className="w-full h-64 rounded-t-lg" />
 
-      <div className="p-6 space-y-3">
-        <Skeleton className="w-24 h-4" />
-        <Skeleton className="w-2/3 h-6" />
+      <div className="p-6 space-y-4">
         <Skeleton className="w-1/3 h-4" />
-        <Skeleton className="w-28 h-5 rounded-full" />
-        <Skeleton className="w-1/2 h-8 mt-4" />
+        <Skeleton className="w-2/3 h-6" />
+        <Skeleton className="w-1/2 h-4" />
+        <Skeleton className="w-1/4 h-6 rounded-full" />
+        <Skeleton className="w-1/3 h-8 mt-4" />
       </div>
     </div>
-  );
+  )
 }
