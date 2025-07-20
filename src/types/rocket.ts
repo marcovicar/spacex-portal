5e830bb6573fc039148c259ef28ce0d5e0791235
marcovicar <marcosvictormbi85@gmail.com>
2025-07-21 00:35:33 +0100

    ✅ Created rocket.ts types (interface)

---
+++ src/types/rocket.ts
@@ -0,0 +1,14 @@
+export interface Rocket {
+  id: string;
+  name: string;
+  type: string;
+  active: boolean;
+  stages: number;
+  boosters: number;
+  cost_per_launch: number;
+  success_rate_pct: number;
+  first_flight: string;
+  country: string;
+  company: string;
+  description: string;
+}
