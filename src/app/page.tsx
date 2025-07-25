0ece80b28271c821da33d02d11d7f14c8df23d76
marcovicar <marcosvictormbi85@gmail.com>
2025-07-25 12:40:19 +0100

    ✅ Improvements on main page.tsx, background galaxy fix for full page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,17 +19,17 @@ export default function HomePage() {
   return (
     <main className="relative min-h-screen overflow-hidden bg-black text-white px-6 py-10 pt-24">
       {/* Background animado */}
-      <div className="absolute inset-0 z-0">
+      <div className="fixed inset-0  h-screen w-screen">
         <Galaxy />
       </div>
 
       <div className="relative z-10 space-y-16">
         {/* Main section */}
         <section className="text-center space-y-4 pt-16">
-          <h1 className="text-5xl font-bebas uppercase tracking-widest animate-in slide-in-from-bottom fade-in duration-700">
+          <h1 className="text-5xl font-bold font-bebas uppercase tracking-widest animate-in slide-in-from-bottom fade-in duration-700">
             Welcome to SpaceX Portal
           </h1>
-          <p className="text-zinc-400 max-w-xl mx-auto text-lg">
+          <p className="text-white max-w-xl mx-auto text-lg">
             Discover the most exciting SpaceX launches and missions ever made.
           </p>
           <Button asChild className="mt-4 border rounded-none cursor-pointer border-white px-4 py-2 text-sm uppercase font-medium text-black bg-white hover:bg-black hover:text-white transition-all duration-200">
@@ -40,7 +40,7 @@ export default function HomePage() {
         {/* About section */}
         <section className="text-center max-w-3xl mx-auto space-y-4 pt-8">
           <h2 className="text-3xl font-semibold uppercase font-bebas">About the Portal</h2>
-          <p className="text-zinc-400 text-base">
+          <p className="text-white text-base">
             {"This portal is a tribute to space exploration powered by the SpaceX API. You can explore previous missions, understand the rocket technology, and dive into humanity's leap toward the stars."}
           </p>
         </section>
